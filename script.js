75cc482aee1a40493d376795bdecf1a3941bc85b
hannasiliuk <110416793+hannasiliuk@users.noreply.github.com>
2022-08-17 19:34:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
+sendInput.addEventListener('keydown', function(event) {
+  if (event.key == 'Enter) {
+      // отправка сообщения
+      }
+})
+
 /* Пустые элементы не должны добавляться */
+
+if (messageText != '') {
+  messagesContainer.append(newMessage);
+}
 
 /* Если кликнуть на элемент списка, он зачеркивается */
 
+			element.classList.add('class'); 
+   
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
+			element.classList.toggle('class'); 
+
 /* Очищать input после добавления нового элемента в список */
+
+    sendInput.value = '';
