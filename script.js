21df3556c5fc4c3f8b65c72b74caeeacd307a35b
hannasiliuk <110416793+hannasiliuk@users.noreply.github.com>
2022-08-20 13:50:27 +0300

    Домашнее задание 4
    Написала код

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const groceriest = document.querySelector('.groceries');
+const items = document.querySelector('.items');
+const secondInput = document.querySelector('#input');
+
+function done() {}
+for (let  of pens) {
+    pen.addEventListener('click', function() {
+        pen.classList.toggle('done');
+
+    });
+}
+
+function addItem() {
+    let item = document.createElement('p');
+    item.textContent = '-' + secondInput.value;
+
+    item.addEventListener('click', function() {
+        item.classList.toggle('done');
+    });
+    if (items != '') {
+        items.append(item);
+    };
+    secondInput.value = '';
+}
+
+secondInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter')
+        addItem();
+});
